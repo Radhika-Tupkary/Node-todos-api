c811c5a789bc27e815e5dfde67355281ea5cccab
Radhika Tupkary <miraclegirl@Radhikas-MacBook-Air.local>
2018-02-24 23:35:28 -0500

    Test POST todos route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.post('/todos', (req, res) => {
   todoSample.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.send(e);
+    res.status(400).send(e);
   });
 
 });
@@ -27,3 +27,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, (req, res) => {
   console.log(`server is up and running on port 3000!`);
 });
+
+module.exports = {app};
